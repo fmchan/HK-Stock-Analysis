7dd28662810b4b72a9a0c67b8ea0795d5455b003
HemanLeung <hemanleung@hket.com>
2021-01-21 16:04:33 +0800

    update linux scripts

--- restart_app.sh
+++ restart_app.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-PID=$(cat /usr/local/apps/stock-analysis/app.pid)
+PID=$(cat /usr/local/apps/stock-analysis/stock-app.pid)
 
 if ! ps -p $PID > /dev/null
 then
-  rm -rf /usr/local/apps/stock-analysis/app.pid
-  nohup /root/miniconda3/envs/stock-analysis/bin/python3.6 /usr/local/apps/stock-analysis/app.py >> stock-analysis.log & echo $! >> /usr/local/apps/stock-analysis/app.pid
+  rm -rf /usr/local/apps/stock-analysis/stock-app.pid
+  nohup /root/miniconda3/envs/stock-analysis/bin/python3.6 /usr/local/apps/stock-analysis/app.py >> stock-app.log & echo $! >> /usr/local/apps/stock-analysis/stock-app.pid
 fi
 
 #*/1 * * * * /usr/local/apps/stock-analysis/restart_app.sh
--- restart_scheduler.sh
+++ restart_scheduler.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-PID=$(cat /usr/local/apps/stock-analysis/app.pid)
+PID=$(cat /usr/local/apps/stock-analysis/stock-scheduler.pid)
 
 if ! ps -p $PID > /dev/null
 then
-  rm -rf /usr/local/apps/stock-analysis/app.pid
-  nohup /root/miniconda3/envs/stock-analysis/bin/python3.6 /usr/local/apps/stock-analysis/scheduler.py >> stock-scheduler.log & echo $! >> /usr/local/apps/stock-analysis/app.pid
+  rm -rf /usr/local/apps/stock-analysis/stock-scheduler.pid
+  nohup /root/miniconda3/envs/stock-analysis/bin/python3.6 /usr/local/apps/stock-analysis/scheduler.py >> stock-scheduler.log & echo $! >> /usr/local/apps/stock-analysis/stock-scheduler.pid
 fi
 
 #*/1 * * * * /usr/local/apps/stock-analysis/restart_scheduler.sh
